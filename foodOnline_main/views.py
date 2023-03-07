eccc6af9f97231d0bfe7578ca22350675e391f65
mk0121 <mahesh.kadam0121@gmail.com>
2023-03-07 23:19:00 +0530

    homepage setup, postgres config anf .env-sampple file

--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(r):
-    return HttpResponse('hello world !')
+def home(request):
+    return render(request, 'home.html')
